92df1d62b75e05b2c91f03a7611747b953d7f8ff
jungju <leejungju.go@gmail.com>
2018-02-27 02:13:15 +0900

    샘플 코드 추가

---
+++ web/editor/src/scenes/Story.js
@@ -0,0 +1,15 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Scene {
+  constructor () {
+    super({ key: 'StoryScene' })
+  }
+
+  preload () {
+  }
+
+  create () {
+  }
+
+  update () {}
+}
