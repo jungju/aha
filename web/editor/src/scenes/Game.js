42b4a0693bcfaf1e83a15bda4500e18bd9b6ac55
jungju <leejungju.go@gmail.com>
2018-02-27 21:48:55 +0900

    필요없는 파일 삭제

--- web/editor/src/scenes/Game.js
+++ web/editor/src/scenes/Game.js
@@ -1,7 +1,7 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 
-import Mushroom from '../sprites/Mushroom'
+import Sprite from '../sprites/Sprite'
 
 export default class extends Phaser.Scene {
   constructor () {
@@ -11,7 +11,7 @@ export default class extends Phaser.Scene {
   preload () {}
 
   create () {
-    this.mushroom = new Mushroom({
+    this.mushroom = new Sprite({
       scene: this,
       x: 400,
       y: 300,
@@ -19,9 +19,9 @@ export default class extends Phaser.Scene {
     })
 
     this.add.existing(this.mushroom)
-    this.add.text(100, 100, 'Phaser 3 - ES6 - Webpack ', {
-      font: '64px Bangers',
-      fill: '#7744ff'
-    })
+    // this.add.text(100, 100, 'Phaser 3 - ES6 - Webpack ', {
+    //   font: '64px Bangers',
+    //   fill: '#7744ff'
+    // })
   }
 }
