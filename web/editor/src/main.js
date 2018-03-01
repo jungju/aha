95f40692154df92160eaf387484c793d80b8a114
jungju <leejungju.go@gmail.com>
2018-03-01 16:15:41 +0900

    테스트 코드

--- web/editor/src/main.js
+++ web/editor/src/main.js
@@ -24,7 +24,6 @@ setDomain("/v1");
 
 // window.game = new Game()
 
-
 var config = {
   type: Phaser.AUTO,
   width: 800,
@@ -42,29 +41,39 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-    function preload (){
-        StoryController_Get_All({}).then(response =>{
-            console.log(response.data)
-        }).catch(error => {
-            
-        });
+function preload (){
+    // StoryController_Get_All({}).then(response =>{
+    //     console.log(response.data)
+    // }).catch(error => {
+        
+    // });
+    this.load.setBaseURL('http://localhost:8081/v1');
+    this.load.json('story', '/stories/1');
 
-        this.load.setBaseURL('http://labs.phaser.io');
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
-    }
-    function create (){
-        this.add.image(400, 300, 'sky');
-        var particles = this.add.particles('red');
-        var emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
-        var logo = this.physics.add.image(400, 100, 'logo');
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-        emitter.startFollow(logo);
-    }
+    var story = this.cache.json.get('story')
+    console.log(story.Name);
+    // this.load.image('sky', 'assets/skies/space3.png');
+    // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    // this.load.image('red', 'assets/particles/red.png');
+}
+
+function create (){
+    var story = this.cache.json.get('story')
+    //console.log(story.Name);
+    this.add.text(100, 100, story.Name, {
+      font: '64px Bangers',
+      fill: '#7744f0'
+    })
+    // this.add.image(400, 300, 'sky');
+    // var particles = this.add.particles('red');
+    // var emitter = particles.createEmitter({
+    //     speed: 100,
+    //     scale: { start: 1, end: 0 },
+    //     blendMode: 'ADD'
+    // });
+    // var logo = this.physics.add.image(400, 100, 'logo');
+    // logo.setVelocity(100, 200);
+    // logo.setBounce(1, 1);
+    // logo.setCollideWorldBounds(true);
+    // emitter.startFollow(logo);
+}
